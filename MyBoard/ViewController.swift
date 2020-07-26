55f0c4b8331f574849b9a642ab714133a8df5d08
jinwei <jinweikim@gmail.com>
2020-07-26 18:05:06 +0800

    Initial Commit

---
+++ MyBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyBoard
+//
+//  Created by 金苇 on 2020/7/26.
+//  Copyright © 2020 nuaaplase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
